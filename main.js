c55057453af44bf0d6030b4f2c20c02df89e0b4a
zgh <ghahremans908@gmil.com>
2025-12-24 10:11:04 +0330

    Error correction

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const name = prompt("نام تان را وارد کنید");
+const Name = prompt("نام تان را وارد کنید");
 const task = prompt("وظیفه را وارد کنید ");
 const clock = Number(prompt(" ساعت هشدار ؟"));
 const main = Number(prompt("دقیقه ؟"));
 
-function createtaskcounter() {
+function makeCounter() {
   let count = 0;
   return {
     increment() {
@@ -24,26 +24,32 @@ let valid = new Proxy(pos, {
       if (value < 0 || value > 23) {
         throw new RangeError("ساعت را اشتباه انتخاب کردید ");
       }
-    } else if (prop === "minute") {
+    } else if (prop === "main") {
       if (value < 0 || value > 59) {
         throw new RangeError("دقیقه را اشتباه انتخاب کردید ");
+      }
+    } else if (prop === "Name") {
+      if (!value || value.trim() === "") {
+        throw new Error("عنوان نام الزامی است");
       }
     } else if (prop === "task") {
       if (!value || value.trim() === "") {
         throw new Error("عنوان وظیفه الزامی است");
-      } else {
-        throw new Error("'cloce' و 'minute' مجاز هستند");
       }
-
-      target[prop] = value;
-
-      return true;
+    } else {
+      throw new Error("'cloce' و 'minute' ,.... مجاز هستند");
     }
+
+    target[prop] = value;
+
+    return true;
   },
 });
 
-valid.cloce = clockInput;
-valid.minute = mainInput;
+valid.cloce = clock;
+valid.minute = main;
+valid.name = Name;
+valid.task = task;
 
 const Times = valid.cloce * 3600000 + valid.minute * 60000;
 
@@ -55,7 +61,7 @@ function Dailytasks(task) {
 
 async function gettask() {
   let data = await Dailytasks();
-  console.log(date);
+  console.log(data);
 }
 
 gettask();
